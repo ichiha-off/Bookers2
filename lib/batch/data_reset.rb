4906748ef309e0aad253bd618ac9479d33f17f60
vagrant <vagrant@localhost.localdomain>
2020-06-08 07:06:26 +0000

    [Updata]バッチの適用

---
+++ lib/batch/data_reset.rb
@@ -0,0 +1,14 @@
+class Batch::DataReset
+    def self.data_reset
+        #投稿全て削除
+        Book.delete_all
+        p "投稿を全て削除しました。"
+    end
+end
+# class Batch::DataReset
+#     def self.data_reset
+#         # 投稿を全て削除
+#         Book.delete_all
+#         p "投稿を全て削除しました"
+#     end
+# end
